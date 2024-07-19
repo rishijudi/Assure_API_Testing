0c7a352bcfff5f864aaa5353cd224a3f1b50a017
vasusekar <vasu.sekar@invicara.com>
2024-07-19 11:25:12 +0530

    Asset End Point Added

--- AssureService_Api_Testing/src/test/java/com/as/qa/test/AssetsOfInterest.java
+++ AssureService_Api_Testing/src/test/java/com/as/qa/test/AssetsOfInterest.java
@@ -13,7 +13,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 
 public class AssetsOfInterest extends BaseSetup {
 
-	@Test(priority = 0)
+	@Test(priority = 0,enabled = false)
 	public void testGetAssureAssetsEndpoint() throws IOException {
 		 String baseUrl = loadProperties().getProperty("BaseURIH");
 	        String assurePath = loadProperties().getProperty("assurePath");
@@ -39,9 +39,9 @@ public class AssetsOfInterest extends BaseSetup {
 	        response.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\get_assure_assets.json"));
 	        long responseTime = response.time();
 	        System.out.println("Response time: " + responseTime + " ms");
-	        Assert.assertTrue(responseTime < 400, "Response time is too slow");
+	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
 	        }
-	@Test(priority = 1)
+	@Test(priority = 1,enabled = false)
 	public void testPostAssureAssetCodesByEquivalencesAndPrimaryClassification() throws IOException {
 		 String baseUrl = loadProperties().getProperty("BaseURIH");
 	        String assurePath = loadProperties().getProperty("assurePath");
@@ -59,4 +59,126 @@ public class AssetsOfInterest extends BaseSetup {
 	        long responseTime = res.time();
 	        System.out.println("Response time: " + responseTime + " ms");
 	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
-}}
+}
+	@Test(priority = 1)//Not Yet Ready
+	public void testGetAssureAssetChildCountByPrimaryClassification() throws IOException {
+		String baseUrl = loadProperties().getProperty("BaseURIH");
+        String assurePath = loadProperties().getProperty("assurePath");
+        String classId = loadProperties().getProperty("ClassId");
+        String nsfilter = loadProperties().getProperty("nsfilter");
+        // Set the endpoint and query parameters
+        String endPointUrl = "/asset_count";
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("primaryClassificationId", classId);
+        queryParams.put("nsfilter", nsfilter);
+        String endpoint = baseUrl + assurePath + endPointUrl;
+        // Send the GET request
+        Response response = getRequest(endpoint, queryParams);
+        // Assert the status code
+        int statusCode = getStatusCode(response);
+        System.out.println(statusCode);
+        String asPrettyString = response.getBody().asString();
+        System.out.println("=========="+asPrettyString);
+        Assert.assertEquals(statusCode, 200, "Expected status code 200");
+        response.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\Get_body\\asset_count.json"));
+        long responseTime = response.time();
+        System.out.println("Response time: " + responseTime + " ms");
+        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
+}
+	@Test(priority = 1,enabled = false)//Not Yet Ready
+	public void testGetAssureAssetsByAssetCodesAndPrimaryClassification() throws IOException {
+		 String baseUrl = loadProperties().getProperty("BaseURIH");
+	        String assurePath = loadProperties().getProperty("assurePath");
+	        String nsfilter = loadProperties().getProperty("nsfilter");
+	        String endPointUrl = "/assetCodesWithEquivalency";
+	        Map<String, String> queryParams1 = new HashMap<>();
+	        queryParams1.put("nsfilter", nsfilter);
+	        String endpoint = baseUrl + assurePath + endPointUrl;
+	        Map<String, Object> body = new HashMap<>();
+	        body.put("primaryClassificationId", "massurervt_2C8q8PXa:m ifc enum");
+	        body.put("equivalencies",new String[]{"m Operations Class"});
+	        Response res = postRequest(endpoint,queryParams1,body);
+	        int statusCode = getStatusCode(res);
+	        res.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\assetsCodesWithEquivalency.json"));
+	        long responseTime = res.time();
+	        System.out.println("Response time: " + responseTime + " ms");
+	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
+}
+	@Test(priority = 1,enabled = false)//Not Yet Ready
+	public void testGetAssureAssetsByQuery() throws IOException {
+		 String baseUrl = loadProperties().getProperty("BaseURIH");
+	        String assurePath = loadProperties().getProperty("assurePath");
+	        String nsfilter = loadProperties().getProperty("nsfilter");
+	        String endPointUrl = "/assetCodesWithEquivalency";
+	        Map<String, String> queryParams1 = new HashMap<>();
+	        queryParams1.put("nsfilter", nsfilter);
+	        String endpoint = baseUrl + assurePath + endPointUrl;
+	        Map<String, Object> body = new HashMap<>();
+	        body.put("primaryClassificationId", "massurervt_2C8q8PXa:m ifc enum");
+	        body.put("equivalencies",new String[]{"m Operations Class"});
+	        Response res = postRequest(endpoint,queryParams1,body);
+	        int statusCode = getStatusCode(res);
+	        res.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\assetsCodesWithEquivalency.json"));
+	        long responseTime = res.time();
+	        System.out.println("Response time: " + responseTime + " ms");
+	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
+}
+	@Test(priority = 1,enabled = false)//Not Yet Ready
+	public void testGetAssureAssetCountByPrimaryClassification() throws IOException {
+		 String baseUrl = loadProperties().getProperty("BaseURIH");
+	        String assurePath = loadProperties().getProperty("assurePath");
+	        String nsfilter = loadProperties().getProperty("nsfilter");
+	        String endPointUrl = "/assetCodesWithEquivalency";
+	        Map<String, String> queryParams1 = new HashMap<>();
+	        queryParams1.put("nsfilter", nsfilter);
+	        String endpoint = baseUrl + assurePath + endPointUrl;
+	        Map<String, Object> body = new HashMap<>();
+	        body.put("primaryClassificationId", "massurervt_2C8q8PXa:m ifc enum");
+	        body.put("equivalencies",new String[]{"m Operations Class"});
+	        Response res = postRequest(endpoint,queryParams1,body);
+	        int statusCode = getStatusCode(res);
+	        res.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\assetsCodesWithEquivalency.json"));
+	        long responseTime = res.time();
+	        System.out.println("Response time: " + responseTime + " ms");
+	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
+}
+	@Test(priority = 1,enabled = false)//Not Yet Ready
+	public void testUpdateAnAsset() throws IOException {
+		 String baseUrl = loadProperties().getProperty("BaseURIH");
+	        String assurePath = loadProperties().getProperty("assurePath");
+	        String nsfilter = loadProperties().getProperty("nsfilter");
+	        String endPointUrl = "/assetCodesWithEquivalency";
+	        Map<String, String> queryParams1 = new HashMap<>();
+	        queryParams1.put("nsfilter", nsfilter);
+	        String endpoint = baseUrl + assurePath + endPointUrl;
+	        Map<String, Object> body = new HashMap<>();
+	        body.put("primaryClassificationId", "massurervt_2C8q8PXa:m ifc enum");
+	        body.put("equivalencies",new String[]{"m Operations Class"});
+	        Response res = postRequest(endpoint,queryParams1,body);
+	        int statusCode = getStatusCode(res);
+	        res.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\assetsCodesWithEquivalency.json"));
+	        long responseTime = res.time();
+	        System.out.println("Response time: " + responseTime + " ms");
+	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
+}
+	@Test(priority = 1,enabled = false)//Not Yet Ready
+	public void testUpdateAsset() throws IOException {
+		 String baseUrl = loadProperties().getProperty("BaseURIH");
+	        String assurePath = loadProperties().getProperty("assurePath");
+	        String nsfilter = loadProperties().getProperty("nsfilter");
+	        String endPointUrl = "/assetCodesWithEquivalency";
+	        Map<String, String> queryParams1 = new HashMap<>();
+	        queryParams1.put("nsfilter", nsfilter);
+	        String endpoint = baseUrl + assurePath + endPointUrl;
+	        Map<String, Object> body = new HashMap<>();
+	        body.put("primaryClassificationId", "massurervt_2C8q8PXa:m ifc enum");
+	        body.put("equivalencies",new String[]{"m Operations Class"});
+	        Response res = postRequest(endpoint,queryParams1,body);
+	        int statusCode = getStatusCode(res);
+	        res.then().assertThat().body(matchesJsonSchemaInClasspath("AssetsOfInterest_Json\\assetsCodesWithEquivalency.json"));
+	        long responseTime = res.time();
+	        System.out.println("Response time: " + responseTime + " ms");
+	        Assert.assertTrue(responseTime < 2000, "Response time is too slow");
+}
+	
+	}
